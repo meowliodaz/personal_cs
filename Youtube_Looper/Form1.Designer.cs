37d7f585fe8d040f9ea141df3ad21c77c31ee984
Trieu <phamhai.trieu@hachi-x.com>
2023-12-20 22:26:19 +0700

    :sparkles: 1st final draft

--- Youtube_Looper/Form1.Designer.cs
+++ Youtube_Looper/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Youtube_Looper;
 
-partial class Form1
+partial class mainForm
 {
     /// <summary>
     ///  Required designer variable.
@@ -28,31 +28,135 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        label1 = new Label();
+        titleLbl = new Label();
+        urlLbl = new Label();
+        urlTxt = new TextBox();
+        loopLbl = new Label();
+        loopTxt = new TextBox();
+        playBtn = new Button();
+        stopBtn = new Button();
+        statusStrip1 = new StatusStrip();
+        downloadStatus = new ToolStripStatusLabel();
+        statusStrip1.SuspendLayout();
         SuspendLayout();
         // 
-        // label1
+        // titleLbl
         // 
-        label1.AutoSize = true;
-        label1.Location = new Point(105, 19);
-        label1.Name = "label1";
-        label1.Size = new Size(91, 15);
-        label1.TabIndex = 0;
-        label1.Text = "Youtube Looper";
+        titleLbl.AutoSize = true;
+        titleLbl.Font = new Font("UD Digi Kyokasho NK-B", 18F, FontStyle.Bold, GraphicsUnit.Point, 128);
+        titleLbl.Location = new Point(300, 20);
+        titleLbl.Name = "titleLbl";
+        titleLbl.Size = new Size(203, 28);
+        titleLbl.TabIndex = 0;
+        titleLbl.Text = "Youtube Looper";
         // 
-        // Form1
+        // urlLbl
+        // 
+        urlLbl.AutoSize = true;
+        urlLbl.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        urlLbl.Location = new Point(20, 100);
+        urlLbl.Name = "urlLbl";
+        urlLbl.Size = new Size(114, 21);
+        urlLbl.TabIndex = 1;
+        urlLbl.Text = "Youtube URL";
+        // 
+        // urlTxt
+        // 
+        urlTxt.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        urlTxt.Location = new Point(180, 97);
+        urlTxt.Name = "urlTxt";
+        urlTxt.Size = new Size(580, 29);
+        urlTxt.TabIndex = 2;
+        // 
+        // loopLbl
+        // 
+        loopLbl.AutoSize = true;
+        loopLbl.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        loopLbl.Location = new Point(20, 160);
+        loopLbl.Name = "loopLbl";
+        loopLbl.Size = new Size(138, 21);
+        loopLbl.TabIndex = 1;
+        loopLbl.Text = "Number of loops";
+        // 
+        // loopTxt
+        // 
+        loopTxt.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        loopTxt.Location = new Point(180, 157);
+        loopTxt.Name = "loopTxt";
+        loopTxt.Size = new Size(80, 29);
+        loopTxt.TabIndex = 2;
+        // 
+        // playBtn
+        // 
+        playBtn.BackColor = Color.FromArgb(128, 255, 128);
+        playBtn.Font = new Font("UD Digi Kyokasho NK-B", 18F, FontStyle.Bold, GraphicsUnit.Point, 128);
+        playBtn.Location = new Point(500, 240);
+        playBtn.Name = "playBtn";
+        playBtn.Size = new Size(120, 55);
+        playBtn.TabIndex = 3;
+        playBtn.Text = "Play";
+        playBtn.UseVisualStyleBackColor = false;
+        playBtn.Click += playBtn_Click;
+        // 
+        // stopBtn
+        // 
+        stopBtn.BackColor = Color.Tomato;
+        stopBtn.Font = new Font("UD Digi Kyokasho NK-B", 18F, FontStyle.Bold, GraphicsUnit.Point, 128);
+        stopBtn.Location = new Point(180, 240);
+        stopBtn.Name = "stopBtn";
+        stopBtn.Size = new Size(120, 55);
+        stopBtn.TabIndex = 3;
+        stopBtn.Text = "Stop";
+        stopBtn.UseVisualStyleBackColor = false;
+        stopBtn.Click += stopBtn_Click;
+        // 
+        // statusStrip1
+        // 
+        statusStrip1.Items.AddRange(new ToolStripItem[] { downloadStatus });
+        statusStrip1.Location = new Point(0, 335);
+        statusStrip1.Name = "statusStrip1";
+        statusStrip1.Size = new Size(784, 26);
+        statusStrip1.TabIndex = 4;
+        statusStrip1.Text = "statusStrip1";
+        // 
+        // downloadStatus
+        // 
+        downloadStatus.BackColor = Color.Transparent;
+        downloadStatus.Font = new Font("Yu Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        downloadStatus.Name = "downloadStatus";
+        downloadStatus.Size = new Size(146, 21);
+        downloadStatus.Text = "Download status...";
+        // 
+        // mainForm
         // 
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(800, 450);
-        Controls.Add(label1);
-        Name = "Form1";
-        Text = "Form1";
+        ClientSize = new Size(784, 361);
+        Controls.Add(statusStrip1);
+        Controls.Add(stopBtn);
+        Controls.Add(playBtn);
+        Controls.Add(loopTxt);
+        Controls.Add(urlTxt);
+        Controls.Add(loopLbl);
+        Controls.Add(urlLbl);
+        Controls.Add(titleLbl);
+        Name = "mainForm";
+        Text = "Youtube Looper";
+        statusStrip1.ResumeLayout(false);
+        statusStrip1.PerformLayout();
         ResumeLayout(false);
         PerformLayout();
     }
 
     #endregion
 
-    private Label label1;
+    private Label titleLbl;
+    private Label urlLbl;
+    private TextBox urlTxt;
+    private Label loopLbl;
+    private TextBox loopTxt;
+    private Button playBtn;
+    private Button stopBtn;
+    private StatusStrip statusStrip1;
+    private ToolStripStatusLabel downloadStatus;
 }
--- Youtube_Looper/Form1.cs
+++ Youtube_Looper/Form1.cs
@@ -1,9 +1,136 @@
+using AngleSharp.Text;
+using NAudio.Wave;
+using System.IO;
+using System.Threading.Tasks;
+
+using YoutubeExplode;
+
+
 namespace Youtube_Looper;
 
-public partial class Form1 : Form
+public partial class mainForm : Form
 {
-    public Form1()
+    private bool running = false;
+    private string videoPath = "";
+
+    public mainForm()
     {
         InitializeComponent();
+    }
+
+    private async Task DownloadYouTubeVideo(string videoUrl, string outputDirectory)
+    {
+        downloadStatus.Text = "Download status...";
+
+        var youtube = new YoutubeClient();
+        var video = await youtube.Videos.GetAsync(videoUrl);
+
+        // Sanitize the video title to remove invalid characters from the file name
+        string sanitizedTitle = string.Join("_", video.Title.Split(Path.GetInvalidFileNameChars()));
+
+        // Get all available muxed streams
+        var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Id);
+        var muxedStreams = streamManifest.GetMuxedStreams().OrderByDescending(s => s.VideoQuality).ToList();
+
+        if (muxedStreams.Any())
+        {
+            var streamInfo = muxedStreams.First();
+            using var httpClient = new HttpClient();
+            var stream = await httpClient.GetStreamAsync(streamInfo.Url);
+
+            string outputFilePath = Path.Combine(outputDirectory, $"{sanitizedTitle}.{streamInfo.Container}");
+
+            if (!File.Exists(outputFilePath))
+            {
+                using var outputStream = File.Create(outputFilePath);
+                await stream.CopyToAsync(outputStream);
+                downloadStatus.Text = "Video downloaded!";
+            }
+            else
+            {
+                downloadStatus.Text = "Video already exists.";
+            }
+
+            videoPath = outputFilePath;
+        }
+        else
+        {
+            downloadStatus.Text = "Can't find/access video.";
+            videoPath = "";
+        }
+    }
+
+    private void PlayAudioFromVideo(string path)
+    {
+        int loop = Int32.Parse(loopTxt.Text);
+        for (int i = 0; i < loop; i++)
+        {
+            using (var waveOut = new WaveOutEvent())
+            {
+                var audioStream = new MediaFoundationReader(path);
+                waveOut.Init(audioStream);
+                waveOut.Play();
+
+                while (waveOut.PlaybackState == PlaybackState.Playing)
+                {
+                    System.Threading.Thread.Sleep(50);
+                    if (!running)
+                    {
+                        break;
+                    }
+                }
+
+                waveOut.Dispose();
+                audioStream.Dispose();
+            }
+
+        }
+    }
+
+    private async Task RunDownloadYouTubeVideo()
+    {
+        // Set the output directory path here
+        string currentDirectory = Directory.GetCurrentDirectory();
+        string outputDirectory = currentDirectory + @"\Downloads";
+        if (!Directory.Exists(outputDirectory)) Directory.CreateDirectory(outputDirectory);
+
+        // List of YouTube video URLs to download
+        List<string> videoUrls = new List<string>
+            {
+                $"{urlTxt.Text}"
+                // Add more video URLs as needed
+            };
+        try
+        {
+            foreach (var videoUrl in videoUrls)
+            {
+                await DownloadYouTubeVideo(videoUrl, outputDirectory);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred while downloading the videos: " + ex.Message);
+        }
+
+        if (videoPath.Length > 0)
+        {
+            downloadStatus.BackColor = Color.Transparent;
+            Task.Run(() => PlayAudioFromVideo(videoPath));
+        }
+        else
+        {
+            downloadStatus.BackColor = Color.Red;
+        }
+    }
+
+    private void playBtn_Click(object sender, EventArgs e)
+    {
+        running = true;
+        RunDownloadYouTubeVideo();
+    }
+
+    private void stopBtn_Click(object sender, EventArgs e)
+    {
+        running = false;
     }
 }
