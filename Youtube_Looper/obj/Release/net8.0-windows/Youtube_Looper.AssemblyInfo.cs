1ca2df2bbe4bf01ed946532fb8f7d8f40835f222
Trieu <phamhai.trieu@hachi-x.com>
2023-12-23 11:51:13 +0700

    :+1: Last config before publish

--- Youtube_Looper/obj/Release/net8.0-windows/Youtube_Looper.AssemblyInfo.cs
+++ Youtube_Looper/obj/Release/net8.0-windows/Youtube_Looper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Youtube_Looper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2882957921e6584b05757199647d8d9fc48ba73d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Youtube_Looper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Youtube_Looper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
